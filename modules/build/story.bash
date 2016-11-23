31a2fd939b3d2195453b46d533a1490db0d4bfa6
Alexey Melezhik <melezhik@gmail.com>
2016-11-23 12:48:53 +0000

    export vars

--- modules/build/story.bash
+++ modules/build/story.bash
@@ -1,4 +1,14 @@
+
+export SRCROOT=$(config srcroot)
+export ARCH=$(config arch)
+export DEBUG=$(config debug)
+
+
 thing=$(story_var thing)
 
 echo build $thing ...
+
+echo SRCROOT: $SRCROOT
+echo DEBUG  : $DEBUG
+echo ARCH   : $ARCH
 
