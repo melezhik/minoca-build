0254185df6f66499654c75b61db287c328c817d3
Alexey Melezhik <melezhik@gmail.com>
2016-11-24 14:20:51 +0300

    add new actions

---
+++ modules/build-image/story.bash
@@ -0,0 +1,26 @@
+
+export SRCROOT=$(config srcroot)
+export ARCH=$(config arch)
+export DEBUG=$(config debug)
+PATH=$PATH:$SRCROOT/$ARCH$DEBUG/tools/bin
+
+echo -n build image ... ' '
+
+#echo SRCROOT: $SRCROOT
+#echo DEBUG  : $DEBUG
+#echo ARCH   : $ARCH
+#echo PATH   : $PATH
+
+
+cd $SRCROOT/os/images && \
+if make clean 1>$test_root_dir/$story_dir/make.report.txt 2>&1 \
+  && make 1>>$test_root_dir/$story_dir/make.report.txt 2>&1; then
+  echo ok
+else
+  echo failed
+  echo last 10 lines at report file: $test_root_dir/$story_dir/make.report.txt
+  tail -n 10 $test_root_dir/$story_dir/make.report.txt
+  exit 1
+fi
+
+ls -l $SRCROOT/$ARCH$DEBUG/bin/*.img
---
+++ modules/build-os/story.bash
@@ -0,0 +1,24 @@
+
+export SRCROOT=$(config srcroot)
+export ARCH=$(config arch)
+export DEBUG=$(config debug)
+PATH=$PATH:$SRCROOT/$ARCH$DEBUG/tools/bin
+
+echo -n build os ... ' '
+
+#echo SRCROOT: $SRCROOT
+#echo DEBUG  : $DEBUG
+#echo ARCH   : $ARCH
+#echo PATH   : $PATH
+
+
+cd $SRCROOT/os && \
+if make 1>$test_root_dir/$story_dir/make.report.txt 2>&1; then
+  echo ok
+else
+  echo failed
+  echo last 10 lines at report file: $test_root_dir/$story_dir/make.report.txt
+  tail -n 10 $test_root_dir/$story_dir/make.report.txt
+  exit 1
+fi
+
