281bce44bda52b9e4ad08ef24a13e1f58285f799
Alexey Melezhik <melezhik@gmail.com>
2016-11-25 14:04:55 +0300

    clean action fixed

--- modules/clean/story.bash
+++ modules/clean/story.bash
@@ -5,6 +5,8 @@ export DEBUG=$(config debug)
 PATH=$PATH:$SRCROOT/$ARCH$DEBUG/tools/bin
 
 thing=$(story_var thing)
+thing_safe_name=$(story_var thing_safe_name)
+export thing
 
 echo -n clean $thing ... ' '
 
@@ -14,13 +16,17 @@ echo -n clean $thing ... ' '
 #echo PATH   : $PATH
 
 
-cd $SRCROOT/third-party/build/$thing && \
-if make clean 1>$test_root_dir/$thing-make-clean.report.txt 2>&1; then
+cd $SRCROOT/third-party || exit 1
+cp Makefile Makefile.new || exit 1
+perl -i -p -e 's/\$\(APPS\)/$ENV{thing}/g' Makefile.new || exit 1
+
+if make -f Makefile.new clean 1>$test_root_dir/$thing_safe_name-make-clean.report.txt 2>&1; then
   echo ok
 else
   echo failed
-  echo last 10 lines at report file: $test_root_dir/$thing-make-clean.report.txt
-  tail -n 10 $test_root_dir/$thing-make-clean.report.txt
+  echo last 10 lines at report file: $test_root_dir/$thing_safe_name-make-clean.report.txt
+  ls -l $test_root_dir/$thing_safe_name-make-clean.report.txt
+  tail -n 10 $test_root_dir/$thing_safe_make-clean.report.txt
   exit 1
 fi
 
